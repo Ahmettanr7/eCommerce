d109bac996e87af5d428217789b1580b9b80e5f9
Ahmettanr7 <69989747+Ahmettanr7@users.noreply.github.com>
2021-08-18 01:22:05 +0300

    Add project files.

---
+++ eCommerce/Controllers/HomePageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eCommerce.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+
+        ETRADE4Entities db = new ETRADE4Entities();
+        public ActionResult Index()
+        {
+            ViewModel model = new ViewModel();
+
+             model.ItemList = db.items.ToList();
+             model.CategoryList = db.category1.ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ eCommerce/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eCommerce.Models.Entity;
+using PagedList;
+using PagedList.Mvc; 
+
+
+namespace eCommerce
+{
+    public class ViewModel
+    {
+        public List<items> ItemList { get; set; }
+
+        public List<category1> CategoryList { get; set; }
+    }
+}
